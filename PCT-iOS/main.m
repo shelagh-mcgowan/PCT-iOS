8d5848d1c748565cc88a6cf6c8b19ce8c9ec2ae4
bellaincanto <shelagh_mcgowan@my.uri.edu>
2014-05-25 17:11:52 -0400

    Initial Commit

---
+++ PCT-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PCT-iOS
+//
+//  Created by Shelagh McGowan on 5/25/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PCT-iOSTests/PCT_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  PCT_iOSTests.m
+//  PCT-iOSTests
+//
+//  Created by Shelagh McGowan on 5/25/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PCT_iOSTests : XCTestCase
+
+@end
+
+@implementation PCT_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
